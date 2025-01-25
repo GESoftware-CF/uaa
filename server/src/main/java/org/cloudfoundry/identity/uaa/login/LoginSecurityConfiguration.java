54985bbf115aaa372563c792c857a540b2910c13
Daniel Garnier-Moiroux <daniel.garnier-moiroux@broadcom.com>
2025-01-25 09:30:23 +0100

    move messagePropertiesSource to java config

--- server/src/main/java/org/cloudfoundry/identity/uaa/login/LoginSecurityConfiguration.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/login/LoginSecurityConfiguration.java
@@ -1,8 +1,17 @@
 package org.cloudfoundry.identity.uaa.login;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.ResourcePropertySource;
+
+import java.io.IOException;
 
 @Configuration
 class LoginSecurityConfiguration {
+
+    @Bean
+    ResourcePropertySource messagePropertiesSource() throws IOException {
+        return new ResourcePropertySource("messages.properties");
+    }
 
 }
