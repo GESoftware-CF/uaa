fc9b9d2ce195c3bd90b6c1508a1ff1d5def803fd
Daniel Garnier-Moiroux <daniel.garnier-moiroux@broadcom.com>
2025-01-26 14:34:09 +0100

    Move KeyInfoService to javaconfig

--- server/src/main/java/org/cloudfoundry/identity/uaa/UaaConfiguration.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/UaaConfiguration.java
@@ -1,9 +1,17 @@
 package org.cloudfoundry.identity.uaa;
 
+import org.cloudfoundry.identity.uaa.oauth.KeyInfoService;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(UaaProperties.class)
 class UaaConfiguration {
+
+    @Bean
+    KeyInfoService keyInfoService(UaaProperties uaaProperties) {
+        return new KeyInfoService(uaaProperties.getUrl());
+    }
+
 }
