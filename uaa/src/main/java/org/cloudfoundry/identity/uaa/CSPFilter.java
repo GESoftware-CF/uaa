6e362ffae18c0f3a5abe71879b357294f2a0358e
Jeyaraj, Johnson <johnson.jeyaraj@ge.com>
2025-02-25 12:26:23 +0530

    Add csp header to uaa
    To fix Bitsight findings on UAA, need to add the following csp headers
    [1]
    
    [1] https://confluence.apps.gevernova.net/devspace/display/~223036055/XSS+-+CSP

---
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
@@ -0,0 +1,36 @@
+package org.cloudfoundry.identity.uaa;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CSPFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Filter initialization if needed
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        httpResponse.setHeader("Content-Security-Policy",
+                               "default-src 'self'; script-src 'self'; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; font-src 'self' https://cdn.predix-ui.com; img-src 'self'; frame-src 'self';");
+
+        // Continue with the next filter in the chain
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // Clean up resources if needed
+    }
+}
