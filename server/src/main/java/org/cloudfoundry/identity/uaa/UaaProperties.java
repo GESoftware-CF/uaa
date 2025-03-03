cf3217ba3386d5c16ee63984b89915ae2b85b76b
Daniel Garnier-Moiroux <daniel.garnier-moiroux@broadcom.com>
2025-03-03 11:35:05 +0100

    Introduce UaaProperties.Servlet

--- server/src/main/java/org/cloudfoundry/identity/uaa/UaaProperties.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/UaaProperties.java
@@ -2,6 +2,8 @@ package org.cloudfoundry.identity.uaa;
 
 import org.cloudfoundry.identity.uaa.util.UaaStringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.lang.Nullable;
 
 /**
  * Future replacement of {@link org.cloudfoundry.identity.uaa.impl.config.UaaConfiguration}
@@ -17,5 +19,13 @@ public class UaaProperties {
             }
         }
     }
+
+    @ConfigurationProperties(prefix = "servlet")
+    public record Servlet(SessionCookie sessionCookie, @DefaultValue("1800") int idleTimeout) {
+    }
+
+    public record SessionCookie(@DefaultValue("true") boolean encodeBase64, @Nullable Integer maxAge) {
+    }
+
 }
 
