b1aa3a3b6d16fcd8d16f2867df7f82cc6762f41f
Daniel Garnier-Moiroux <daniel.garnier-moiroux@broadcom.com>
2025-03-12 15:44:07 +0100

    FilterChainOrder: fix existing order, prepare structure, improve javadoc

--- server/src/main/java/org/cloudfoundry/identity/uaa/web/FilterChainOrder.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/web/FilterChainOrder.java
@@ -3,13 +3,34 @@ package org.cloudfoundry.identity.uaa.web;
 /**
  * The order for all the filter chains in the UAA. The name references
  * Spring Security's {@code FilterOrderRegistration}.
+ * <p>
+ * This class allows us to retain the implicit filter chain ordering that we had in
+ * {@code spring-servlet.xml}. The specific order is computed like so:
+ * {@code 100 * (position of file in spring-servlet) + (position of filter chain in file)}.
  */
 public class FilterChainOrder {
 
-    public static final int SCIM_PASSWORD = 400;
-    public static final int SCIM = 401;
+    // login-server-security.xml: 100
+
+    // oauth-endpoints.xml: 200
+
+    // scim-endpoints.xml: 300
+    public static final int SCIM_PASSWORD = 300;
+    public static final int SCIM = 301;
+
+    // multitenant-endpoints.xml: 400
+
+    // approval-endpoints.xml: 500
+
+    // client-admin-endpoints.xml: 600
+
+    // resource-endpoints.xml: 700
     public static final int RATE_LIMIT = 700;
+
+    // openid-endpoints.xml: 800
     public static final int USERINFO = 800;
+
+    // codestore-endpoints.xml: 900
     public static final int CODESTORE = 900;
 
     // login-ui.xml: 1200
