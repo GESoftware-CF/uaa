9546b0c2e76026017483dcb148edd13f1c7ace5b
Filip Hanik <fhanik@pivotal.io>
2025-04-22 15:57:09 -0700

    WIP [spring-servlet.xml] - phase 3 - CorsFilter with a lot of config

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/SpringServletXml.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/SpringServletXml.java
@@ -5,7 +5,10 @@ import org.cloudfoundry.identity.uaa.impl.config.UaaConfiguration;
 import org.cloudfoundry.identity.uaa.impl.config.YamlConfigurationValidator;
 import org.cloudfoundry.identity.uaa.oauth.DisableIdTokenResponseTypeFilter;
 import org.cloudfoundry.identity.uaa.oauth.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.cloudfoundry.identity.uaa.security.web.CorsFilter;
 import org.cloudfoundry.identity.uaa.web.BackwardsCompatibleScopeParsingFilter;
+import org.cloudfoundry.identity.uaa.zone.beans.IdentityZoneManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +24,12 @@ import java.util.Arrays;
 @Configuration
 @ComponentScan("org.cloudfoundry.identity.uaa")
 public class SpringServletXml {
+
+    @Autowired
+    CorsProperties corsProperties;
+
+    @Autowired
+    IdentityZoneManager identityZoneManager;
 
     @Bean
     BackwardsCompatibleScopeParsingFilter backwardsCompatibleScopeParameter() {
@@ -75,6 +84,26 @@ public class SpringServletXml {
     @Bean
     UTF8ConversionFilter utf8ConversionFilter() {
         return new UTF8ConversionFilter();
+    }
+
+    @Bean
+    CorsFilter corsFilter() {
+        CorsFilter bean = new CorsFilter(identityZoneManager, corsProperties.enforceSystemZoneSettings);
+
+        bean.setCorsAllowedUris(corsProperties.defaultAllowed.uris());
+        bean.setCorsAllowedOrigins(corsProperties.defaultAllowed.origins());
+        bean.setCorsAllowedHeaders(corsProperties.defaultAllowed.headers());
+        bean.setCorsAllowedMethods(corsProperties.defaultAllowed.methods());
+        bean.setCorsAllowedCredentials(corsProperties.defaultAllowed.credentials());
+        bean.setCorsMaxAge(corsProperties.defaultMaxAge);
+
+        bean.setCorsXhrAllowedUris(corsProperties.xhrAllowed.uris());
+        bean.setCorsXhrAllowedOrigins(corsProperties.xhrAllowed.origins());
+        bean.setCorsXhrAllowedHeaders(corsProperties.xhrAllowed.headers());
+        bean.setCorsXhrAllowedMethods(corsProperties.xhrAllowed.methods());
+        bean.setCorsXhrAllowedCredentials(corsProperties.xhrAllowed.credentials());
+        bean.setCorsXhrMaxAge(corsProperties.xhrMaxAge);
+        return bean;
     }
 
 }
