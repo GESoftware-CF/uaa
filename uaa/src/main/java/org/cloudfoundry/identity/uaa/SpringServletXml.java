c21c466652e3a0f1090ba507528a1eb46ccebf6c
Filip Hanik <fhanik@vmware.com>
2025-04-23 06:45:58 -0700

    WIP [spring-servlet.xml] - phase 4 - minor beans

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/SpringServletXml.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/SpringServletXml.java
@@ -1,12 +1,31 @@
 package org.cloudfoundry.identity.uaa;
 
 import org.cloudfoundry.identity.uaa.authentication.UTF8ConversionFilter;
+import org.cloudfoundry.identity.uaa.authentication.UaaAuthenticationDetailsSource;
+import org.cloudfoundry.identity.uaa.authentication.UaaExceptionTranslator;
+import org.cloudfoundry.identity.uaa.cache.UrlContentCache;
+import org.cloudfoundry.identity.uaa.client.ClientAdminEndpointsValidator;
+import org.cloudfoundry.identity.uaa.client.event.ClientAdminEventPublisher;
 import org.cloudfoundry.identity.uaa.impl.config.UaaConfiguration;
 import org.cloudfoundry.identity.uaa.impl.config.YamlConfigurationValidator;
+import org.cloudfoundry.identity.uaa.login.Prompt;
 import org.cloudfoundry.identity.uaa.oauth.DisableIdTokenResponseTypeFilter;
+import org.cloudfoundry.identity.uaa.oauth.provider.ClientDetails;
 import org.cloudfoundry.identity.uaa.oauth.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.cloudfoundry.identity.uaa.oauth.provider.error.OAuth2AuthenticationEntryPoint;
+import org.cloudfoundry.identity.uaa.oauth.provider.error.WebResponseExceptionTranslator;
+import org.cloudfoundry.identity.uaa.oauth.provider.vote.ScopeVoter;
+import org.cloudfoundry.identity.uaa.provider.oauth.OidcMetadataFetcher;
+import org.cloudfoundry.identity.uaa.resources.QueryableResourceManager;
+import org.cloudfoundry.identity.uaa.security.ContextSensitiveOAuth2WebSecurityExpressionHandler;
+import org.cloudfoundry.identity.uaa.security.beans.SecurityContextAccessor;
 import org.cloudfoundry.identity.uaa.security.web.CorsFilter;
 import org.cloudfoundry.identity.uaa.web.BackwardsCompatibleScopeParsingFilter;
+import org.cloudfoundry.identity.uaa.zone.ClientSecretPolicy;
+import org.cloudfoundry.identity.uaa.zone.ClientSecretValidator;
+import org.cloudfoundry.identity.uaa.zone.Links;
+import org.cloudfoundry.identity.uaa.zone.MultitenantClientServices;
+import org.cloudfoundry.identity.uaa.zone.ZoneAwareClientSecretPolicyValidator;
 import org.cloudfoundry.identity.uaa.zone.beans.IdentityZoneManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,11 +34,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AuthenticatedVoter;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.access.vote.UnanimousBased;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.session.ChangeSessionIdAuthenticationStrategy;
 import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @ComponentScan("org.cloudfoundry.identity.uaa")
@@ -103,6 +132,163 @@ public class SpringServletXml {
         bean.setCorsXhrAllowedMethods(corsProperties.xhrAllowed.methods());
         bean.setCorsXhrAllowedCredentials(corsProperties.xhrAllowed.credentials());
         bean.setCorsXhrMaxAge(corsProperties.xhrMaxAge);
+        return bean;
+    }
+
+    @Bean
+    Boolean allowQueryStringForTokens(@Value("${jwt.token.queryString.enabled:true}") boolean enabled) {
+        return enabled;
+    }
+
+    @Bean
+    ChangeSessionIdAuthenticationStrategy sessionFixationProtectionStrategy() {
+        return new ChangeSessionIdAuthenticationStrategy();
+    }
+    
+    @Bean
+    List<String> defaultFilteredHeaders() {
+        return Arrays.asList(
+                "X-Forwarded-For",
+                "X-Forwarded-Host",
+                "X-Forwarded-Proto",
+                "X-Forwarded-Prefix",
+                "Forwarded"
+        );
+    }
+
+    @Bean
+    String uaaUrl(@Value("${uaa.url:http://localhost:8080/uaa}") String uaaUrl) {
+        return uaaUrl;
+    }
+
+    @Bean
+    String loginUrl(@Value("${login.url:http://localhost:8080/uaa}") String loginUrl) {
+        return loginUrl;
+    }
+
+    @Bean
+    ClientAdminEndpointsValidator clientDetailsValidator(
+            SecurityContextAccessor securityContextAccessor,
+            @Qualifier("clientDetailsService") QueryableResourceManager<ClientDetails> clientDetailsService,
+            @Qualifier("zoneAwareClientSecretPolicyValidator") ClientSecretValidator clientDetailsValidator) {
+        ClientAdminEndpointsValidator bean = new ClientAdminEndpointsValidator(securityContextAccessor);
+        bean.setClientDetailsService(clientDetailsService);
+        bean.setClientSecretValidator(clientDetailsValidator);
+        return bean;
+
+    }
+
+    @Bean
+    ClientAdminEventPublisher clientAdminEventPublisher(
+            MultitenantClientServices clientDetailsService
+    ) {
+        return new ClientAdminEventPublisher(clientDetailsService, identityZoneManager);
+    }
+
+    @Bean
+    ZoneAwareClientSecretPolicyValidator zoneAwareClientSecretPolicyValidator(
+            @Qualifier("globalClientSecretPolicy") ClientSecretPolicy clientSecretPolicy
+    ) {
+        return new ZoneAwareClientSecretPolicyValidator(clientSecretPolicy);
+    }
+
+    @Bean
+    ContextSensitiveOAuth2WebSecurityExpressionHandler oauthWebExpressionHandler() {
+        return new ContextSensitiveOAuth2WebSecurityExpressionHandler();
+    }
+
+    @Bean
+    ReloadableResourceBundleMessageSource messageSource() {
+        String basenames = System.getenv().get("CLOUDFOUNDRY_CONFIG_PATH");
+        if (basenames != null) {
+            basenames = "file:" + basenames;
+        } else {
+            basenames = "classpath:messages";
+        }
+        ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+        bean.setBasenames(basenames, "classpath:messages");
+        return bean;
+    }
+
+    @Bean
+    UaaAuthenticationDetailsSource authenticationDetailsSource() {
+        return new UaaAuthenticationDetailsSource();
+    }
+
+    @Bean
+    UaaExceptionTranslator accountNotVerifiedExceptionTranslator() {
+        return new UaaExceptionTranslator();
+    }
+
+    @Bean
+    OAuth2AuthenticationEntryPoint basicAuthenticationEntryPoint(
+            @Qualifier("accountNotVerifiedExceptionTranslator") WebResponseExceptionTranslator<?> exceptionTranslator
+    ) {
+        OAuth2AuthenticationEntryPoint bean = new OAuth2AuthenticationEntryPoint();
+        bean.setRealmName("UAA/client");
+        bean.setTypeName("Basic");
+        bean.setExceptionTranslator(exceptionTranslator);
+        return bean;
+    }
+
+    @Bean
+    OAuth2AuthenticationEntryPoint oauthAuthenticationEntryPoint() {
+        OAuth2AuthenticationEntryPoint bean = new OAuth2AuthenticationEntryPoint();
+        bean.setRealmName("UAA/oauth");
+        return bean;
+    }
+
+    @Bean
+    UnanimousBased accessDecisionManager() {
+        List<AccessDecisionVoter<?>> voters = new ArrayList<>();
+        ScopeVoter scopeVoter = new ScopeVoter();
+        scopeVoter.setScopePrefix("scope=");
+        voters.add(scopeVoter);
+        voters.add(new RoleVoter());
+        voters.add(new AuthenticatedVoter());
+        return new UnanimousBased(voters);
+    }
+
+    @Bean
+    Http403ForbiddenEntryPoint http403EntryPoint() {
+        return new Http403ForbiddenEntryPoint();
+    }
+
+    @Bean
+    List<Prompt> prompts(
+            @Value("${login.prompt.username.text:Email}") String username,
+            @Value("${login.prompt.password.text:Password}") String password,
+            @Value("${login.entityBaseURL:http://localhost:8080/uaa}") String passcode
+    ) {
+        return Arrays.asList(
+                new Prompt("username", "text", username),
+                new Prompt("password", "password", password),
+                new Prompt("passcode", "password",
+                        "Temporary Authentication Code ( Get one at "+passcode+"/passcode )")
+        );
+    }
+
+    @Bean
+    OidcMetadataFetcher oidcMetadataFetcher(
+            UrlContentCache contentCache,
+            @Qualifier("trustingRestTemplate")RestTemplate trustingRestTemplate,
+            @Qualifier("nonTrustingRestTemplate") RestTemplate nonTrustingRestTemplate
+    ) {
+        return new OidcMetadataFetcher(contentCache, trustingRestTemplate, nonTrustingRestTemplate);
+    }
+
+    @Bean
+    Links globalLinks(
+            @Value("${links.global.passwd:#{null}}") String passwd,
+            @Value("${links.global.signup:#{null}}") String signup,
+            @Value("${links.global.homeRedirect:#{null}}") String homeRedirect
+    ) {
+        Links bean = new Links();
+        Links.SelfService selfService = new Links.SelfService();
+        selfService.setPasswd(passwd);
+        selfService.setSignup(signup);
+        bean.setSelfService(selfService);
+        bean.setHomeRedirect(homeRedirect);
         return bean;
     }
 
