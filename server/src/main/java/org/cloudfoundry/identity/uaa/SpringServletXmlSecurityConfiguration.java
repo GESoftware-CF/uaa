be27ceeda47e88617e2e72227483349835a6ad6d
Filip Hanik <fhanik@vmware.com>
2025-06-03 11:10:18 -0700

    Do not send the `X-Frame-Options: DENY` header on the no sec filters
    UAA Singular relies on frames

--- server/src/main/java/org/cloudfoundry/identity/uaa/SpringServletXmlSecurityConfiguration.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/SpringServletXmlSecurityConfiguration.java
@@ -113,6 +113,7 @@ public class SpringServletXmlSecurityConfiguration {
     @Order(FilterChainOrder.NO_SECURITY)
     UaaFilterChain noSecurityFilters(HttpSecurity http) throws Exception {
         SecurityFilterChain chain = http
+                .headers(headers -> headers.frameOptions().disable())
                 .securityMatcher(noSecurityEndpoints)
                 .authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
                 .anonymous(AnonymousConfigurer::disable)
