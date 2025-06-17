1c42721f433ab01ab49932c118104aa2e5838b21
260004147 <260004147@ge.com>
2025-06-17 12:27:34 +0530

    Added csp form-action 'self' with url

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
@@ -23,7 +23,7 @@ public class CSPFilter implements Filter {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
 httpResponse.setHeader("Content-Security-Policy",
-        "default-src 'self'; script-src 'self' https://*.zscalertwo.net; object-src 'none'; style-src 'self'; form-action 'self'; base-uri 'self'; frame-ancestors 'none'; font-src 'self' https://cdn.predix-ui.com; img-src 'self'; frame-src 'self';");
+        "default-src 'self'; script-src 'self' https://*.zscalertwo.net; object-src 'none'; style-src 'self'; form-action 'self' https://fssfed.ge.com/fss/idp/SSO.saml2; base-uri 'self'; frame-ancestors 'none'; font-src 'self' https://cdn.predix-ui.com; img-src 'self'; frame-src 'self';");
 //Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'none'; style-src 'self'; form-action 'self'; base-uri 'self'; frame-ancestors 'none'; font-src 'self' https://cdn.predix-ui.com; img-src 'self'; frame-src 'self'";
         // Continue with the next filter in the chain
         chain.doFilter(request, response);
