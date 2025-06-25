48e1d6ede67c9cba0b5460ddda315c665f3a16d8
260004147 <260004147@ge.com>
2025-06-25 11:42:21 +0530

    Added csp read only

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
@@ -22,8 +22,8 @@ public class CSPFilter implements Filter {
 
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-httpResponse.setHeader("Content-Security-Policy",
-        "default-src 'self'; base-uri 'self'; frame-ancestors 'none'; font-src 'self' https://cdn.predix-ui.com; img-src 'self'; frame-src 'self';");
+httpResponse.setHeader("Content-Security-Policy-Report-Only",
+        "default-src 'self'; script-src 'self'; object-src 'none'; style-src 'self'; form-action 'self'; base-uri 'self'; frame-ancestors 'none'; font-src 'self' https://cdn.predix-ui.com; img-src 'self'; frame-src 'self';");
 //Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'none'; style-src 'self'; form-action 'self'; base-uri 'self'; frame-ancestors 'none'; font-src 'self' https://cdn.predix-ui.com; img-src 'self'; frame-src 'self'";
         // Continue with the next filter in the chain
         chain.doFilter(request, response);
