229b35fa7ab73f899b6cb57c282e3e24968b2824
subh-dash <subhasish.dash@gevernova.com>
2025-08-01 14:29:47 +0530

    removed extra space

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
@@ -18,7 +18,7 @@ public class CSPFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+        throws IOException, ServletException {
 
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
