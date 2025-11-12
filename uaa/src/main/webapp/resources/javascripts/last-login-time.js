82e023fe96e846bfb5edf0214a9822dfb5fccbd6
260004147 <260004147@ge.com>
2025-11-12 15:17:34 +0530

    Fix style-src - 5

---
+++ uaa/src/main/webapp/resources/javascripts/last-login-time.js
@@ -0,0 +1,22 @@
+// Format last login time to user's local timezone
+(function() {
+    'use strict';
+    
+    function formatLastLoginTime() {
+        var element = document.getElementById('last_login_time');
+        if (element && element.hasAttribute('data-timestamp')) {
+            var timestamp = parseInt(element.getAttribute('data-timestamp'), 10);
+            if (!isNaN(timestamp)) {
+                var date = new Date(timestamp);
+                element.textContent = date.toLocaleString();
+            }
+        }
+    }
+    
+    // Execute when DOM is ready
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', formatLastLoginTime);
+    } else {
+        formatLastLoginTime();
+    }
+})();
