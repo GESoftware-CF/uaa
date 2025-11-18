74f430b248762b8e1d3d79050d1d3256e06df5de
Jeyaraj, Johnson <johnson.jeyaraj@ge.com>
2025-11-18 11:50:08 +0530

    Add conditional column additions for backward compatibility in SQL scripts
    Need this for backward compatibility. Need to deploy both old and new
    code in the same environment. So flyway tables will be changed for old
    and new environment. This change needed when we deploy the database
    which already these columns are available.

--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_104__Add_ClientJwtConfig_To_Oauth_Client_Details.sql
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_104__Add_ClientJwtConfig_To_Oauth_Client_Details.sql
@@ -1,2 +1,9 @@
--- NOOP
--- ALTER TABLE oauth_client_details ADD COLUMN client_jwt_config TEXT DEFAULT NULL;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name='oauth_client_details' AND column_name='client_jwt_config'
+    ) THEN
+ALTER TABLE oauth_client_details ADD COLUMN client_jwt_config TEXT DEFAULT NULL;
+END IF;
+END$$;
--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_110__Add_ExternalKey_To_Identity_Provider.sql
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_110__Add_ExternalKey_To_Identity_Provider.sql
@@ -1,3 +1,12 @@
 -- add column external_key for oauth2,oidc,saml2 IdPs
--- ALTER TABLE identity_provider ADD COLUMN external_key VARCHAR(512) DEFAULT NULL;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name='identity_provider' AND column_name='external_key'
+    ) THEN
+ALTER TABLE identity_provider ADD COLUMN external_key VARCHAR(512) DEFAULT NULL;
+END IF;
+END$$;
+
 CREATE UNIQUE INDEX IF NOT EXISTS external_key_in_zone on identity_provider (identity_zone_id,type,external_key) WHERE external_key IS NOT NULL;
