f65254c6888ca5c4f3985408377032dd24f9ed16
260004147 <260004147@ge.com>
2025-11-25 17:49:29 +0530

    remove data from image-src

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/CSPFilter.java
@@ -53,7 +53,7 @@ public class CSPFilter implements Filter {
                 "base-uri 'self'; " +
                 "frame-ancestors 'none'; " +
                 "font-src 'self' https://cdn.predix-ui.com; " +
-                "img-src 'self'; " +
+                "img-src 'self' data:; " +
                 "frame-src 'self';");
 
         // Build report-uri part only if cspReportUri is not empty
